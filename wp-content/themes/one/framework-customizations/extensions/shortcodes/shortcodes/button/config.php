3fe2b427a46ecce4b320ec558256b1bf1685f43e
borodkoleg <borodkoleg@gmail.com>
2018-10-14 22:49:02 +0300

    fonts+, 3 plugins+

--- wp-content/themes/one/framework-customizations/extensions/shortcodes/shortcodes/button/config.php
+++ wp-content/themes/one/framework-customizations/extensions/shortcodes/shortcodes/button/config.php
@@ -8,7 +8,5 @@ $cfg = array(
   'description'   => __('', '{domain}'),
   'tab'           => __('Custom elements', '{domain}'),
   'popup_size'    => 'small', // can be large, medium or small
-  'icon' => get_template_directory_uri().'/framework-customizations/'.
-  'extensions/shortcodes/shortcodes/global_button/static/img/btn.jpg'
   )
 );
