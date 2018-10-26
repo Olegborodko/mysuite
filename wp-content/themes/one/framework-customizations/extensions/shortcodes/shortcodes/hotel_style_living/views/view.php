f1d662e2d1cba7f228cf4a19d6af2f194d1239ee
borodkoleg <borodkoleg@gmail.com>
2018-10-27 01:46:01 +0300

    hotel_style+, fix 3 elements

--- wp-content/themes/one/framework-customizations/extensions/shortcodes/shortcodes/hotel_style_living/views/view.php
+++ wp-content/themes/one/framework-customizations/extensions/shortcodes/shortcodes/hotel_style_living/views/view.php
@@ -35,23 +35,41 @@ if ( !empty( $atts['image_ico'] ) && !empty($atts['image_ico']['url']) ) {
     <div class="hotel_style_living__ico">
       <img src="<?= $image_ico ?>"/>
     </div>
-    <h2 class="af_h2">
-      <?= $atts['title'] ?>
-    </h2>
-    <div data-desktop="<?= $atts['desktop_text_1'] ?>"
-         data-ipad="<?= $atts['ipad_text_1'] ?>"
-         data-mobile="<?= $atts['mobile_text_1'] ?>"
-         class="js_change_text hotel_style_living__text_1 af_type_1_avenir_light">
+
+    <div class="hotel_style_living__h2_block">
+      <div class="hotel_style_living__h2_before">
+        <div class="hotel_style_living__h2_before_border"></div>
+      </div>
+      <h2 class="af_h2 hotel_style_living__h2_center">
+        <?= $atts['title'] ?>
+      </h2>
+      <div class="hotel_style_living__h2_after">
+        <div class="hotel_style_living__h2_after_border"></div>
+      </div>
     </div>
-    <div class="hotel_style_living__images_block">
-      <img class="hotel_style_living__image" src="<?= $image_1 ?>"/>
-      <img class="hotel_style_living__image" src="<?= $image_2 ?>"/>
-      <img class="hotel_style_living__image" src="<?= $image_3 ?>"/>
-    </div>
-    <div data-desktop="<?= $atts['desktop_text_2'] ?>"
-         data-ipad="<?= $atts['ipad_text_2'] ?>"
-         data-mobile="<?= $atts['mobile_text_2'] ?>"
-         class="js_change_text hotel_style_living__text_2 af_type_1_avenir_light">
+
+    <div class="hotel_style_living__block2">
+      <div data-desktop="<?= $atts['desktop_text_1'] ?>"
+           data-ipad="<?= $atts['ipad_text_1'] ?>"
+           data-mobile="<?= $atts['mobile_text_1'] ?>"
+           class="js_change_text hotel_style_living__text_1 af_type_1_avenir_light">
+      </div>
+      <div class="hotel_style_living__images_block">
+        <div class="hotel_style_living__image_div">
+          <img class="hotel_style_living__image" src="<?= $image_1 ?>"/>
+        </div>
+        <div class="hotel_style_living__image_div">
+          <img class="hotel_style_living__image" src="<?= $image_2 ?>"/>
+        </div>
+        <div class="hotel_style_living__image_div">
+          <img class="hotel_style_living__image" src="<?= $image_3 ?>"/>
+        </div>
+      </div>
+      <div data-desktop="<?= $atts['desktop_text_2'] ?>"
+           data-ipad="<?= $atts['ipad_text_2'] ?>"
+           data-mobile="<?= $atts['mobile_text_2'] ?>"
+           class="js_change_text hotel_style_living__text_2 af_type_1_avenir_light">
+      </div>
     </div>
   </div>
 
