1d12c905aa2e5562f46ec8a4dd19945bb9526bb6
borodkoleg <borodkoleg@gmail.com>
2018-11-28 01:13:35 +0200

    filter images hover+

--- wp-content/themes/one/framework-customizations/extensions/shortcodes/shortcodes/filter/views/results.php
+++ wp-content/themes/one/framework-customizations/extensions/shortcodes/shortcodes/filter/views/results.php
@@ -1,7 +1,22 @@
 <div class="filter__result_blocks">
 
   <div class="filter__r_item">
-    <img src="<?= get_stylesheet_directory_uri() ?>/img/b2.jpg">
+    <div class="filter__r_item_block_hover">
+      <img class="filter__r_item_img_block_hover" src="<?= get_stylesheet_directory_uri() ?>/img/b2.jpg">
+      <div class="filter__r_item_a_hover_link_background"></div>
+      <a href="http://test/#test" class="filter__r_item_a_hover_center">
+        Learn More
+        <div class="filter__r_item_a_hover_arrow"></div>
+        <div class="filter__r_item_a_hover_bath">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bathtub.png">
+          3
+        </div>
+        <div class="filter__r_item_a_hover_bed">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bed.png">
+          2
+        </div>
+      </a>
+    </div>
     <div class="filter__r_description">
       <div class="filter__r_description_left">
         <div class="filter__r_item_location">
@@ -23,7 +38,25 @@
   </div>
 
   <div class="filter__r_item">
-    <img src="<?= get_stylesheet_directory_uri() ?>/img/b1.jpg">
+    <div class="filter__r_item_block_hover">
+      <div class="filter__r_premium">
+        <img src="<?= get_stylesheet_directory_uri() ?>/img/star_white.png"> One Month free!
+      </div>
+      <img class="filter__r_item_img_block_hover" src="<?= get_stylesheet_directory_uri() ?>/img/b1.jpg">
+      <div class="filter__r_item_a_hover_link_background"></div>
+      <a href="http://test/#test" class="filter__r_item_a_hover_center">
+        Learn More
+        <div class="filter__r_item_a_hover_arrow"></div>
+        <div class="filter__r_item_a_hover_bath">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bathtub.png">
+          3
+        </div>
+        <div class="filter__r_item_a_hover_bed">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bed.png">
+          3
+        </div>
+      </a>
+    </div>
     <div class="filter__r_description">
       <div class="filter__r_description_left">
         <div class="filter__r_item_location">
@@ -41,41 +74,29 @@
           Semi-private
         </div>
       </div>
-    </div>
-
-    <div class="filter__r_premium">
-      <img src="<?= get_stylesheet_directory_uri() ?>/img/star_white.png"> One Month free!
     </div>
   </div>
 
   <div class="filter__r_item">
-    <img src="<?= get_stylesheet_directory_uri() ?>/img/b3.jpg">
-    <div class="filter__r_description">
-      <div class="filter__r_description_left">
-        <div class="filter__r_item_location">
-          Mayfield Avenue, Los Angeles
-        </div>
-        <div class="filter__r_item_title">
-          Luxe Villas
-        </div>
-        <div class="filter__r_item_price">
-          $200 per day
-        </div>
+    <div class="filter__r_item_block_hover">
+      <div class="filter__r_premium">
+        <img src="<?= get_stylesheet_directory_uri() ?>/img/star_white.png"> One Month free!
       </div>
-      <div class="filter__r_description_right">
-        <div class="filter__r_button">
-          Semi-private
+      <img class="filter__r_item_img_block_hover" src="<?= get_stylesheet_directory_uri() ?>/img/b3.jpg">
+      <div class="filter__r_item_a_hover_link_background"></div>
+      <a href="http://test/#test" class="filter__r_item_a_hover_center">
+        Learn More
+        <div class="filter__r_item_a_hover_arrow"></div>
+        <div class="filter__r_item_a_hover_bath">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bathtub.png">
+          4
         </div>
-      </div>
+        <div class="filter__r_item_a_hover_bed">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bed.png">
+          1
+        </div>
+      </a>
     </div>
-
-    <div class="filter__r_premium">
-      <img src="<?= get_stylesheet_directory_uri() ?>/img/star_white.png"> One Month free!
-    </div>
-  </div>
-
-  <div class="filter__r_item">
-    <img src="<?= get_stylesheet_directory_uri() ?>/img/b4.jpg">
     <div class="filter__r_description">
       <div class="filter__r_description_left">
         <div class="filter__r_item_location">
@@ -95,5 +116,44 @@
       </div>
     </div>
   </div>
+
+  <div class="filter__r_item">
+    <div class="filter__r_item_block_hover">
+      <img class="filter__r_item_img_block_hover" src="<?= get_stylesheet_directory_uri() ?>/img/b4.jpg">
+      <div class="filter__r_item_a_hover_link_background"></div>
+      <a href="http://test/#test" class="filter__r_item_a_hover_center">
+        Learn More
+        <div class="filter__r_item_a_hover_arrow"></div>
+        <div class="filter__r_item_a_hover_bath">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bathtub.png">
+          3
+        </div>
+        <div class="filter__r_item_a_hover_bed">
+          <img src="<?= get_stylesheet_directory_uri() ?>/img/bed.png">
+          1
+        </div>
+      </a>
+    </div>
+    <div class="filter__r_description">
+      <div class="filter__r_description_left">
+        <div class="filter__r_item_location">
+          Mayfield Avenue, Los Angeles
+        </div>
+        <div class="filter__r_item_title">
+          Luxe Villas
+        </div>
+        <div class="filter__r_item_price">
+          $200 per day
+        </div>
+      </div>
+      <div class="filter__r_description_right">
+        <div class="filter__r_button">
+          Semi-private
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 
 </div>
